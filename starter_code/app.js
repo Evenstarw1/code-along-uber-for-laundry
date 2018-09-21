9673c9ef193d96e101e5784c17ab21e9b06ec68c
Evenstarw1 <lauracorrallopez@gmail.com>
2018-09-21 13:50:23 +0200

    Finished

--- starter_code/app.js
+++ starter_code/app.js
@@ -8,6 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const index = require('./routes/index');
+const authRoutes = require('./routes/auth');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const laundryRoutes = require('./routes/laundry');
 
 
 mongoose.Promise = Promise;
@@ -29,6 +34,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'never do your own laundry again',
+  resave: true,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+app.use((req, res, next) => {
+  if (req.session.currentUser) {
+    res.locals.currentUserInfo = req.session.currentUser;
+    res.locals.isUserLoggedIn = true;
+  } else {
+    res.locals.isUserLoggedIn = false;
+  }
+
+  next();
+});
+app.use('/', index);
+app.use('/', authRoutes);
+
 
 // Express View engine setup
 
@@ -51,8 +80,10 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 
 
-const index = require('./routes/index');
 app.use('/', index);
+app.use('/', authRoutes);
+app.use('/', laundryRoutes);
+
 
 
 module.exports = app;
